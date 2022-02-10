ce61e3d1d5269ab58aff752f63b322b680497fd5
kouha.shu <kouha.shu@gmail.com>
2022-02-10 17:31:06 +0900

    save

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get 'welcome/bs'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
   resources :articles do
